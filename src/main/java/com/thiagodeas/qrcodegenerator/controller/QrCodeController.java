bb1e2fcb430b5de47f50119d39bf080b3e5916c2
thiagodeas <sousaalvesth@gmail.com>
2025-06-23 10:27:10 -0300

    feat(controller): implement qrcode generation endpoint

--- src/main/java/com/thiagodeas/qrcodegenerator/controller/QrCodeController.java
+++ src/main/java/com/thiagodeas/qrcodegenerator/controller/QrCodeController.java
@@ -8,13 +8,25 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.thiagodeas.qrcodegenerator.dto.qrcode.QrCodeGenerateRequest;
 import com.thiagodeas.qrcodegenerator.dto.qrcode.QrCodeGenerateResponse;
+import com.thiagodeas.qrcodegenerator.service.QrCodeGeneratorService;
 
 @RestController
 @RequestMapping("/qrcode")
 public class QrCodeController {
 
+    private final QrCodeGeneratorService qrCodeGeneratorService;
+
+    public QrCodeController(QrCodeGeneratorService qrCodeService) {
+        this.qrCodeGeneratorService = qrCodeService;
+    }
+
     @PostMapping
     public ResponseEntity<QrCodeGenerateResponse> generate(@RequestBody QrCodeGenerateRequest request) {
-        return null;
+        try {
+            QrCodeGenerateResponse response = this.qrCodeGeneratorService.generateAndUploadQrCode(request.text());
+            return ResponseEntity.ok(response);
+        } catch (Exception e) {
+            return ResponseEntity.internalServerError().build();
+        }
     }
 }
